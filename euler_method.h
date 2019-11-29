fd37095bb9ffe6338caff576e7de39575a8d00f7
androidmop <57952951+androidmop@users.noreply.github.com>
2019-11-29 09:36:43 +0100

    Create euler_method.h

---
+++ euler_method.h
@@ -0,0 +1,2 @@
+void euler_method(double complex *out, double complex *in, long int N, unsigned int D, long int L, double tau, double total_time);
+void euler_method_single(double complex *out, double complex *in, long int N, unsigned int D, long int L, double tau);
