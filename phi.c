560b5f29964093201597922746e9b78ba3d40d90
duezelbi <56878577+duezelbi@users.noreply.github.com>
2019-12-03 01:25:09 +0100

    added two additional trial functions, changed to cosines to keep periodicity condition

--- phi.c
+++ phi.c
@@ -1,41 +1,51 @@
 #include <complex.h>
 #include <stdlib.h>
 #include <stdio.h>
+##include <math.h>
 #include "indeces.h"
 #include "vmath.h"
 
 void phi(double complex *out, long int N, long int D, long int L, int type){
 	long int *phi_coords;
-    phi_coords = malloc(D*sizeof(long));
-	
+  phi_coords = malloc(D*sizeof(long));
+
 	if (type==0){
 		/* initialise wave function with ones */
 		for(i=0;i<L:i++){
 			out[i]=1
-			}
-		/* wave function as product of sines */
-		for(i=0;i<L;i++){
-			index2coord(phi_coords,i,N,D);
-			for(j=0;j<D;j++){
-				out[i]*=sin(phi_coords[j]);
-				}
-			}
-		double normalization_factor=abs_vec(out,L);
-		for(i=0;i<L;i++){
-			/* normalization of the wave function */
-			out[i]=out[i]/normalization_factor;
-			}
-	if (type==1){
-		/* wave function as square of coordinates */
-		for(i=0;i<L;i++){
-			index2coord(phi_coords,i,N,D);
-			for(j=0;j<D;j++){
-				out[i]+=j*j*ipow(I);
-				}
 		}
-	if (type==2){
-		
+		/* wave function as product of cosines */
+		for(i=0;i<L;i++){
+			index2coord(phi_coords,i,N,D);
+			for(j=0;j<D;j++){
+				out[i]*=cosines(phi_coords[j]);
+			}
 		}
 	}
 
+	if (type==1){
+		/* wave function as square of coordinates with fixed imaginary parts*/
+		for(i=0;i<L;i++){
+			index2coord(phi_coords,i,N,D);
+			for(j=0;j<D;j++){
+				out[i]+=phi_coords[j]*phi_coords[j]*pow(I,j);
+			}
+		}
+	}
 
+	if (type==2){
+		/* wave function as 1/abs(phi_coords) with fixed imaginary parts*/
+		for(i=0;i<L;i++){
+			index2coord(phi_coords,i,N,D);
+			out[i]+=1/abs_vec(phi_coords,D)*pow(I,j);
+		}
+	}
+
+	double normalization_factor=abs_vec(out,L);
+  for(i=0;i<L;i++){
+		/* normalization of the wave function */
+		out[i]=out[i]/normalization_factor;
+	}
+
+	free phi_coords
+}
