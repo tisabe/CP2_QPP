6f84eb066369d8d51da9f9a671b76b166acb85af
IDungeon <57952984+IDungeon@users.noreply.github.com>
2019-12-03 19:12:58 +0100

    potentials now includes three different potetnials

--- potentials.c
+++ potentials.c
@@ -14,22 +14,40 @@ double harmonic0(long int index, int omega, int N, int D) {
     free(coordinates);
     return potential;
 }
-
 void harmonic(double * harmonicpotential, int omega, int N, int D){
     for (int i=0; i<ipow(N, D); i++) {
         harmonicpotential[i]=harmonic0( i, omega,N, D);
     }
 }
 
-double boxpotential0(long int index, int omega, int N, int D) {
+
+
+double boxpotential0(long int index, int height, int N, int D) {
     long int * coordinates = malloc(D* sizeof(long int));
     index2coord(coordinates,index, N, D);
-    int count=1;
     long int boxy=0;
     for (int i=0; i<D; i++) {
         if(faps(coordinates[i])<N/4){
-            boxy=boxy+boxy;
+            boxy=boxy;
         else
             boxy=height;
+            }
+    free(coordinates);
+    return boxy;
+void box(double * boxpotential, int height, int N, int D){
+    for (int i=0; i<ipow(N, D); i++) {
+        boxpotential[i]=boxpotential0( i, height, N, D);
+    }
+}
+
+
+
+double potentialwell(int height, int N, int D) {
+    long int * boxy= malloc(ipow(N, D) * sizeof(long int));
+    for (int i=0; i<D; i++) {
+        if(i>N/4+N * int (i/N) && i< 3/4*N+N * int (i/N)){
+            boxy[i]=0;
+        else
+            boxy[i]=height;
             }
     return boxy;
