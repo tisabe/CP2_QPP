885401b4188831ae00aa355e3cfbd9ef9458b446
Birkan Duezel <duezelbi@pool1.physik.hu-berlin.de>
2019-12-10 16:14:40 +0100

    corrections in potentials.c

--- potentials.c
+++ potentials.c
@@ -3,18 +3,18 @@
 #include <stdlib.h>
 #include <math.h>
 
-double harmonic0(long int index, int omega, int N, int D) {
+double harmonic0(long int index, double omega, long int N, unsigned int D) {
     long int * coordinates = malloc(D* sizeof(long int));
     index2coord(coordinates,index, N, D);
     double potential = 0;
     for (int i=0; i<D; i++) {
-        potential = potential+ipow(coordinates[i], 2);
+        potential += pow(coordinates[i], 2);
   }
-    potential=0.5*ipow(omega, 2)*potential;
+    potential=0.5*pow(omega, 2)*potential;
     free(coordinates);
     return potential;
 }
-void harmonic(double * harmonicpotential, int omega, int N, int D){
+void harmonic(double *harmonicpotential, double omega, long int N, unsigned int D){
     for (int i=0; i<ipow(N, D); i++) {
         harmonicpotential[i]=harmonic0( i, omega,N, D);
     }
@@ -22,19 +22,22 @@ void harmonic(double * harmonicpotential, int omega, int N, int D){
 
 
 
-double boxpotential0(long int index, int height, int N, int D) {
+double boxpotential0(long int index, int height, long int N, unsigned int D) {
     long int * coordinates = malloc(D* sizeof(long int));
     index2coord(coordinates,index, N, D);
     long int boxy=0;
     for (int i=0; i<D; i++) {
-        if(faps(coordinates[i])<N/4){
+        if(fabs(coordinates[i])<N/4){
             boxy=boxy;
         else
             boxy=height;
+            break;
             }
     free(coordinates);
     return boxy;
-void box(double * boxpotential, int height, int N, int D){
+  }
+}
+void box(double *boxpotential, int height, long int N, unsigned int D){
     for (int i=0; i<ipow(N, D); i++) {
         boxpotential[i]=boxpotential0( i, height, N, D);
     }
@@ -42,7 +45,7 @@ void box(double * boxpotential, int height, int N, int D){
 
 
 
-double potentialwell(int height, int N, int D) {
+double potentialwell(int height, long int N, unsigned int D) {
     long int * boxy= malloc(ipow(N, D) * sizeof(long int));
     for (int i=0; i<D; i++) {
         if(i>N/4+N * int (i/N) && i< 3/4*N+N * int (i/N)){
@@ -51,3 +54,11 @@ double potentialwell(int height, int N, int D) {
             boxy[i]=height;
             }
     return boxy;
+  }
+}
+void well(double *wellpotential, int height, long int N, unsigned int D){
+  for (int i=0; i<ipow(N, D); i++) {
+      wellpotential[i]=potentialwell( i, height, N, D);
+  }
+}
+
