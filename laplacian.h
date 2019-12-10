421563f80f2e42b51a39a8ac19117299643c9432
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-10 22:03:52 +0100

    Update as neighbour array is included in laplacian now

--- laplacian.h
+++ laplacian.h
@@ -1,1 +1,1 @@
-void laplacian(double complex *out, long int *neighbours, double complex *in, long int N, unsigned int D, long int L);
+void laplacian(double complex *out, double complex *in, long int N, unsigned int D, long int L);
