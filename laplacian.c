b249a33b5471a9f29c0d26e3877260b3f2d80d45
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-10 22:03:13 +0100

    Static array for next neighbours included

--- laplacian.c
+++ laplacian.c
@@ -2,16 +2,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "nneighbour.h"
+#include "vmath.h"
 
 // Function assumes that *out and *in are already allocated as double complex arrays of length L=N**D
 
-void laplacian(double complex *out, long int *neighbours, double complex *in, long int N, unsigned int D, long int L){
-    /*using the next neighbours saved in a static variable*/
+void laplacian(double complex *out, double complex *in, long int N, unsigned int D){
+    static long int *neighbours = NULL;
+    static long int L, prev_N, prev_D;
+
+    //Check if parameters for neighbours array have changed. If yes, calculate the array again
+    if((neighbours == NULL) || (prev_N != N) || (prev_D != D)){
+        L = ipow(N,D);
+        neighbours = malloc(2*D*L*sizeof(long int));
+        nneighbour_init(neighbours,N,D);
+        prev_N = N;
+        prev_D = D;
+    }
+
+    //Loop over all indices
     for(int i=0; i<L; i++){
         // Loop over all dimensions
         for(int d=0; d<D; d++){
             // Calculate discrete second derivative according to d^2 psi/ dx^2 = (f(x+h)-2*f(x)+f(x-h))/h^2
-            out[i] += (in[neighbours[(i*2*D+2*d)]]-2*in[i]+in[neighbours[(i*2*D+2*d)+1]]);
+            out[i] += (in[neighbours[2*(i*D+d)]]-2*in[i]+in[neighbours[2*(i*D+d)+1]]);
         }
     }
 }
