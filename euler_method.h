a00067965d6d3f4dc4d9a4651790c3585f5ca5b2
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-11 12:02:03 +0100

    Bug fixes to work with hamiltonian

--- euler_method.h
+++ euler_method.h
@@ -1,2 +1,2 @@
-void euler_method(double complex *out, double complex *in, long int N, unsigned int D, long int L, double tau, double total_time);
+void euler_method(double complex *out, double complex *in, long int N, unsigned int D, long int L, double tauhat, double total_time, double m, double epsilon, int ext_potential_type, double parameter);
 void euler_method_single(double complex *out, double complex *in, long int N, unsigned int D, long int L, double tau);
