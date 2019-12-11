3038d634bd1173c709d00deaef3aa27b926830f0
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-11 12:02:25 +0100

    Neighbours removed, laplacian does it by itself

--- hamiltonian.c
+++ hamiltonian.c
@@ -3,28 +3,28 @@
 #include "kinetic.h"
 #include "potentials.h"
 
-void hamiltonian(double complex *out, long int *neighbours, double complex *in, long int N, unsigned int D, long int L, double m, double epsilon, int ext_potential_type, double parameter){
+void hamiltonian(double complex *out, double complex *in, long int N, unsigned int D, long int L, double m, double epsilon, int ext_potential_type, double parameter){
   /*Calculate the kinetic part*/
   double complex *phi_kinetic= malloc(L*sizeof(double complex));
-  kinetic(phi_kinetic,neighbours,in,N,D,L,m);
+  kinetic(phi_kinetic,in,N,D,L,m);
 
   /*Calculate the harmonic part*/
   double *phi_potential= malloc(L*sizeof(double));
-  
+
   if (ext_potential_type==0) {
 	  harmonic(phi_potential,parameter,N,D);
 	  }
-	  
+
   else if (ext_potential_type==1) {
 	  box(phi_potential,parameter,N,D);
 	  }
-  
-  
+
+
   else if (ext_potential_type==2) {
 	  well(phi_potential,parameter,N,D);
 	  }
-  
-  
+
+
 
   /*Calculte the Hamiltonian of in */
   for (int i=0; i<L; i++) {
