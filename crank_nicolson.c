2131f36c227f9a79dcd631884f2d79ff1dd0cc02
tisabe <bechtelt@physik.hu-berlin.de>
2019-12-11 14:47:25 +0100

    contd time step function, not finished

--- crank_nicolson.c
+++ crank_nicolson.c
@@ -4,7 +4,7 @@
 #include "hamiltonian.h"
 
 static void f_eta(double complex *out_eta, double complex *in_eta, long int L, double tau) {
-  hamiltonian(out_eta, in_eta, );
+  hamiltonian(out_eta, in_eta, ); // hier muessen noch parameter eingefuegt werden, vielleicht als typedef struct
   hamiltonian(out_eta, out_eta, );
   scalar_vec(out_eta, out_eta, (double complex) tau*tau/4, L);
   add_vec(out_eta, out_eta, in_eta, L);
@@ -19,5 +19,6 @@ static void f_psi(double complex *out_psi, double complex *in_psi, long int L, d
 
 void step_cn(double complex *out, double complex *in, long int N, unsigned int D, double tau, int max_iter, double tol) {
   long int L = ipow(N, D);
-  
+  cg(out, f_eta, in, max_iter, tol, L);
+  f_psi(out, out, L, tau);
 }
