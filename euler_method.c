dd9bec3f8fd618e42e1dd8288681b3c2db6b068c
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-11 12:01:45 +0100

    Various bug fixes, eg communication with hamiltonian module

--- euler_method.c
+++ euler_method.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <complex.h>
 
-#include "hamiltonian.h"        //Still to be implemented
+#include "hamiltonian.h"
 
 /*  Compute the time propagation according to the Euler method.
     Note: Normalisation of the wave function is not conserved!! */
@@ -11,35 +11,24 @@
 
 /* ************      To be tested        ************ */
 
-void euler_method(double complex *out, double complex *in, long int N, unsigned int D, long int L, double tauhat, double total_time){
+void euler_method(double complex *out, double complex *in, long int N, unsigned int D, long int L, double tauhat, double total_time, double m, double epsilon, int ext_potential_type, double parameter) {
     // Allocate memory for a temporary array of length L
     double complex *ham_out = malloc(L*sizeof(double complex));
 
     // Copy data from in array to out array (= 0 time steps calculated)
-    for(int i=0; i<L, i++){
+    for(long int i=0; i<L; i++){
         out[i] = in[i];
     }
 
     // For loop that is being executed until the simulation length specified in total_length (to be given in 1/E_H approx 2.4 * 10^{-17} s) is reached
     for(int t=0; (t * tauhat) < total_time; t++){
         // Calculate H(psi)
-        hamiltonian(ham_out,out,N,D,L);
+        hamiltonian(ham_out, out, N, D, L, m, epsilon, ext_potential_type, parameter);
         // psi^{q+1} = psi^q - i tau H(psi^q)
-        out -= 1Im * tauhat * ham_out;
+        for(long int i=0; i<L; i++){
+            out[i] -= 1I * tauhat * ham_out[i];
+        }
     }
-
-    free(ham_out);
-}
-
-void euler_method_single(double complex *out, double complex *in, long int N, unsigned int D, long int L, double tau){
-    // Allocate memory for H(psi)
-    double complex *ham_out = malloc(L*sizeof(double complex));
-
-    // Calculate H(psi)
-    hamiltonian(ham_out,in,N,D,L);
-
-    // psi^{q+1} = psi^q - i tau H(psi^q)
-    out = in - 1Im * tau * ham_out;
 
     free(ham_out);
 }
