f6ee2b183fe6d0ee736221495dc476dcfc51d996
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-14 11:56:21 +0100

    Update cg_test.c

--- cg_test.c
+++ cg_test.c
@@ -6,10 +6,8 @@
 
 
 void matrix_generator(double complex *out, double complex *in, long int L){
-  out[0] = 4.0 + 0*I;
-  out[1] = 1.0 + 0*I;
-  out[2] = 1.0 + 0*I;
-  out[3] = 3.0 + 0*I;
+  out[0] = 4.0 * in[0] + 1.0 * in[1];
+  out[1] = 1.0 * in[0] + 3.0 * in[1];
 }
 
 void wikipedia_test(){
@@ -17,8 +15,8 @@ void wikipedia_test(){
 
   long int N = 2;
   unsigned int D = 2;
-  int max_iter = 10;
-  double tol = 0.0000001;
+  int max_iter = 100;
+  double tol = 1e-15;
   long int L = ipow(N,D);
 
   double complex *out = malloc(L*sizeof(double complex));
@@ -26,8 +24,8 @@ void wikipedia_test(){
   //double complex *out_func = malloc(L*sizeof(double complex));
   //double complex *in_func = malloc(N*sizeof(double complex));
 
-  in[0] = 1 + 0*I;
-  in[1] = 2 + 0*I;
+  in[0] = 1.0;
+  in[1] = 2.0;
 
   cg(out, matrix_generator, in, max_iter, tol, L);
 
