803958b98e70d26686200652103ce94a6efc2a51
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-15 21:52:53 +0100

    Update euler_method.h

--- euler_method.h
+++ euler_method.h
@@ -1,2 +1,1 @@
-void euler_method(double complex *out, double complex *in, long int N, unsigned int D, long int L, double tauhat, double total_time, double m, double epsilon, int ext_potential_type, double parameter);
-void euler_method_single(double complex *out, double complex *in, long int N, unsigned int D, long int L, double tau);
+void euler_method(double complex *out, double complex *in, parameters params);
