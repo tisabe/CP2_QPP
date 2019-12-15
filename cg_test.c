86df324eb173ad3c1a30e278de0190ddcb55298f
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-15 18:44:02 +0100

    Comments added

--- cg_test.c
+++ cg_test.c
@@ -9,49 +9,6 @@
 #include "vmath.h"
 
 
-void matrix_generator_exp(double complex *out, double complex *in, parameters params){
-  // function rewritten by Tim for use of struct
-  static double complex matrix[4] = {4.0, 1.0, 1.0, 3.0};
-  mat_vec_mul(out, matrix, in, params.N);
-}
-
-void wikipedia_test_exp(){
-  // function rewritten by Tim for use of struct
-  // [[4,1],[1,3]]*[1/11,7/11]=[1,2]
-  parameters params;
-  params.N = 2;
-  params.D = 2;
-  params.max_iter = 100;
-  params.tol = DBL_EPSILON;
-  params.L = ipow(params.N,params.D);
-
-  double complex *out = malloc(params.L*sizeof(double complex));
-  double complex *in = malloc(params.N*sizeof(double complex));
-  double complex *check_res = malloc(params.N*sizeof(double complex));
-  double *diff = malloc(params.N*sizeof(double complex));
-
-  in[0] = 1.0;
-  in[1] = 2.0;
-
-  cg(out, matrix_generator_exp, in, params);
-
-  printf("[%lf+%lf i, %lf+%lf i]\n",creal(out[0]),cimag(out[0]),creal(out[1]),cimag(out[1]));
-
-  matrix_generator_exp(check_res, out, params);
-  for(int i=0; i<params.N; i++){
-    diff[i] = cabs(check_res[i]-in[i]);
-  }
-  printf("Diff: [%e, %e]\n",diff[0],diff[1]);
-
-  free(out);
-  free(in);
-  free(diff);
-  free(check_res);
-}
-
-
-/*Include some good random number generator here*/
-
 double random_double(){
     return (double)rand()/RAND_MAX;
 }
@@ -110,32 +67,38 @@ void random_matrix(double complex *out, double complex *in, parameters params){
 }
 
 void random_matrix_test(){
-
+  //Set simulation parameters
   parameters params;
-  params.N = 10000;
+  params.N = 10000;                     //Set length of one side of the matrix here
   params.D = 1;
-  params.max_iter = 10000;
-  params.tol = DBL_EPSILON;
+  params.max_iter = 100;                //Set the maximum number of iterations of the cg algorithm
+  params.tol = DBL_EPSILON;             //Set the tolerance level to quit the cg algorithm. Best possible: DBL_PRECISION (macro from float.h)
   params.L = ipow(params.N,params.D);
 
+  //Allocate arrays for computation
   double complex *result_cg = malloc(params.N*sizeof(double complex));
   double complex *start_vec = malloc(params.N*sizeof(double complex));
   double complex *check_res = malloc(params.N*sizeof(double complex));
-
+  //Variable to store maximum relative error of the result. See below
   double max_error = 0.0;
 
+  //Generate a random vector b
   for(int i=0; i<params.N; i++){
     start_vec[i] = (double complex)(rand()-0.5*RAND_MAX + (rand()-0.5*RAND_MAX) * I);
   }
 
+  //Call the conjugate gradient algorithm to calculate x in Ax=b. Store the result in result_cg
   cg(result_cg, random_matrix, start_vec, params);
 
+  //Calculate the matrix vector product of the random matrix A with the solution vector x from cg
   random_matrix(check_res, result_cg, params);
 
-  printf("\nMax_Error(A*x-b) = \n");
+  //Calculate the maximum relative error of the solution with respect to the analytical solution.
+  //max_error = max( ((Ax)_i-b_i)/b_i )
+  printf("\nMaximum Relative Error max(((Ax)_i-b_i)/b_i) = \n");
   for(int i=0; i<params.N; i++){
-    if(cabs(check_res[i]-start_vec[i]) > max_error){
-        max_error = cabs(check_res[i]-start_vec[i]);
+    if(cabs(check_res[i]/start_vec[i]-1) > max_error){
+        max_error = cabs(check_res[i]/start_vec[i]-1);
     }
   }
   printf("%e\n",max_error);
