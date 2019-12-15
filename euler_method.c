70545ef6075ba7358a287c520d604660a78bee76
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-15 21:52:48 +0100

    Update euler_method.c

--- euler_method.c
+++ euler_method.c
@@ -3,6 +3,7 @@
 #include <complex.h>
 
 #include "hamiltonian.h"
+#include "structs.h"
 
 /*  Compute the time propagation according to the Euler method.
     Note: Normalisation of the wave function is not conserved!! */
@@ -11,22 +12,22 @@
 
 /* ************      To be tested        ************ */
 
-void euler_method(double complex *out, double complex *in, long int N, unsigned int D, long int L, double tauhat, double total_time, double m, double epsilon, int ext_potential_type, double parameter) {
+void euler_method(double complex *out, double complex *in, parameters params) {
     // Allocate memory for a temporary array of length L
-    double complex *ham_out = malloc(L*sizeof(double complex));
+    double complex *ham_out = malloc(params.L*sizeof(double complex));
 
     // Copy data from in array to out array (= 0 time steps calculated)
-    for(long int i=0; i<L; i++){
+    for(long int i=0; i<params.L; i++){
         out[i] = in[i];
     }
 
     // For loop that is being executed until the simulation length specified in total_length (to be given in 1/E_H approx 2.4 * 10^{-17} s) is reached
-    for(int t=0; (t * tauhat) < total_time; t++){
+    for(int t=0; (t * params.tau) < params.total_time; t++){
         // Calculate H(psi)
-        hamiltonian(ham_out, out, N, D, L, m, epsilon, ext_potential_type, parameter);
+        hamiltonian(ham_out, out, params.N, params.D, params.L, params.mhat, params.epsilon, params.ext_potential_type, params.parameter);
         // psi^{q+1} = psi^q - i tau H(psi^q)
-        for(long int i=0; i<L; i++){
-            out[i] -= 1I * tauhat * ham_out[i];
+        for(long int i=0; i<params.L; i++){
+            out[i] -= 1I * params.tau * ham_out[i];
         }
     }
 
