da2ee587acd6dfbedbf2eddf406fe855de15dd91
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-15 23:44:25 +0100

    Bugfix in scalar_vec

--- vmath.h
+++ vmath.h
@@ -2,7 +2,7 @@
 
 long int ipow(long int base, unsigned int exp);
 double complex dot_product(double complex *v, double complex *w, long int L);
-void scalar_vec(double complex *out, double complex *vec, double complex *s, long int L);
+void scalar_vec(double complex *out, double complex *vec, double complex s, long int L);
 void mul_element(double complex *out, double complex *w, double complex *v, long int L);
 void add_vec(double complex *out, double complex *w, double complex *v, long int L);
 void sub_vec(double complex *out, double complex *w, double complex *v, long int L);
