f4c406ee95b9919fa515f728243ec5a189ad9d4d
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-18 10:08:40 +0100

    Update euler_method.c

--- euler_method.c
+++ euler_method.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <complex.h>
 
-#include "hamiltonian.h"
 #include "structs.h"
+#include "hamiltonian.h"
 
 /*  Compute the time propagation according to the Euler method.
     Note: Normalisation of the wave function is not conserved!! */
@@ -24,7 +24,7 @@ void euler_method(double complex *out, double complex *in, parameters params) {
     // For loop that is being executed until the simulation length specified in total_length (to be given in 1/E_H approx 2.4 * 10^{-17} s) is reached
     for(int t=0; (t * params.tau) < params.total_time; t++){
         // Calculate H(psi)
-        hamiltonian(ham_out, out, params.N, params.D, params.L, params.mhat, params.epsilon, params.ext_potential_type, params.parameter);
+        hamiltonian(ham_out, out, params);
         // psi^{q+1} = psi^q - i tau H(psi^q)
         for(long int i=0; i<params.L; i++){
             out[i] -= 1I * params.tau * ham_out[i];
