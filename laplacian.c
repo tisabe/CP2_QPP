91d4936cfa00f804e550b318a75c91fec4786834
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-18 10:09:04 +0100

    Undo parameters

--- laplacian.c
+++ laplacian.c
@@ -1,32 +1,32 @@
 #include <complex.h>
 #include <stdlib.h>
 #include <stdio.h>
-#iclude "structs.h"
+
 #include "nneighbour.h"
 #include "vmath.h"
 
 // Function assumes that *out and *in are already allocated as double complex arrays of length L=N**D
 
-void laplacian(double complex *out, double complex *in, parameters params){
+void laplacian(double complex *out, double complex *in, long int N, unsigned int D){
     static long int *neighbours = NULL;
     static long int prev_N, prev_D;
 
     //Check if parameters for neighbours array have changed. If yes, calculate the array again
-    if((neighbours == NULL) || (prev_N != params.N) || (prev_D != params.D)){
+    if((neighbours == NULL) || (prev_N != N) || (prev_D != D)){
         free(neighbours);
-        params.L = ipow(params.N,params.D);
-        neighbours = malloc(2*params.D*params.L*sizeof(long int));
-        nneighbour_init(neighbours, params.N, params.D);
-        prev_N = params.N;
-        prev_D = params.D;
+        long int L = ipow(N, D);
+        neighbours = malloc(2*D*L*sizeof(long int));
+        nneighbour_init(neighbours, N, D);
+        prev_N = N;
+        prev_D = D;
     }
 
     //Loop over all indices
-    for(int i=0; i<params.L; i++){
+    for(int i=0; i<L; i++){
         // Loop over all dimensions
-        for(int d=0; d<params.D; d++){
+        for(int d=0; d<D; d++){
             // Calculate discrete second derivative according to d^2 psi/ dx^2 = (f(x+h)-2*f(x)+f(x-h))/h^2
-            out[i] += (in[neighbours[2*(i*params.D+d)]]-2*in[i]+in[neighbours[2*(i*params.D+d)+1]]);
+            out[i] += (in[neighbours[2*(i*D+d)]]-2*in[i]+in[neighbours[2*(i*D+d)+1]]);
         }
     }
 }
