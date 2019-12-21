bf88610cb7d4e0109a88f12552da697bf99626d2
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-21 18:54:31 +0100

    Create geometry.h

---
+++ geometry.h
@@ -0,0 +1,4 @@
+long int coord2index(long int *coord, long int N, unsigned int D);
+void index2coord(long int *coord, long int index, long int N, unsigned int D);
+long int nneighbour(long int index, unsigned int axis, int dir, long int N, unsigned int D);
+void nneighbour_init(long int *out, long int N, unsigned int D);
