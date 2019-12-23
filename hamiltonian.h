e0b467c19ac8d587ed3eaff2a669687502f6cff4
duezelbi <56878577+duezelbi@users.noreply.github.com>
2019-12-23 17:22:40 +0100

    adapted .h parts to new functions

--- hamiltonian.h
+++ hamiltonian.h
@@ -1,2 +1,3 @@
 void hamiltonian(double complex *out, double complex *in, parameters params);
 void hamiltonian_exp(double complex *out, double complex *in, parameters params);
+void generate_pot(parameters params, double parameter);
--- potentials.h
+++ potentials.h
@@ -1,6 +1,3 @@
-double harmonic0(long int index, double omega,long int N, unsigned int D);
-void harmonic(double *harmonicpotential, double omega,long int N,unsigned int D);
-double boxpotential0(long int index,long int height, int N,unsigned int D);
-void box(double *boxpotential,long int height, int N,unsigned int D);
-double potentialwell(long int i, int height, long int N,unsigned int D);
-void well(double *wellpotential, int height, long int N, unsigned int D);
+void well(double *wellpotential, parameters params, double height);
+void box(double *boxpotential, parameters params, double height);
+void harmonic(double *harmonicpotential, parameters params, double omega);
