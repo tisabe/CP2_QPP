140adb526001bc0f15f91c6f91ed948bde2bac43
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-24 10:40:58 +0100

    length scale added to parameters

--- structs.h
+++ structs.h
@@ -7,6 +7,7 @@ typedef struct parameters_tag {
   long int N; // length of each axis, number of points along each axis
   unsigned int D; // number of dimensions
   long int L; // length of whole array
+  double a; // length scale
   double complex *pot; // space to store potential array
   double tau; // length of time step
   double mhat; // natural parameter m^ = m*epsilon*a^2/hbar^2
@@ -14,7 +15,6 @@ typedef struct parameters_tag {
   long int max_iter; // maximum number of iterations to perform (atm. for to cg method)
   double total_time; //Time that the simulation should run for
   double epsilon; //Energy scale
-  double parameter; //Omega in a harmonic potential
   int ext_potential_type; //External potential type
   double sigma; //Sigma in a gaussian distribution
   long int *phi0; //Center in a gaussian distribution
