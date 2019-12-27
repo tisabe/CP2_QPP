ebe6d532018a6d685d0ef8ada8ff5650a3b639d5
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-27 17:31:34 +0100

    Update to new hamiltionian function

--- crank_nicolson.c
+++ crank_nicolson.c
@@ -7,18 +7,18 @@
 
 static void f_eta(double complex *out_eta, double complex *in_eta, parameters params) {
   double complex *temp = malloc(params.L * sizeof(double complex));
-  hamiltonian_exp(temp, in_eta, params);
-  hamiltonian_exp(out_eta, temp, params);
-  scalar_vec(out_eta, out_eta, (double complex) params.tau*params.tau/4, params.L);
+  hamiltonian(temp, in_eta, params);
+  hamiltonian(out_eta, temp, params);
+  scalar_vec(out_eta, out_eta, (double complex) params.tauhat*params.tauhat/4, params.L);
   add_vec(out_eta, out_eta, in_eta, params.L);
   free(temp);
 }
 
 static void f_psi(double complex *out_psi, double complex *in_psi, parameters params) {
   double complex *temp = malloc(params.L * sizeof(double complex));
-  hamiltonian_exp(temp, in_psi, params);
-  hamiltonian_exp(out_psi, temp, params);
-  scalar_vec(out_psi, out_psi, (-1)*1I*params.tau/2, params.L);
+  hamiltonian(temp, in_psi, params);
+  hamiltonian(out_psi, temp, params);
+  scalar_vec(out_psi, out_psi, (-1)*1I*params.tauhat/2, params.L);
   add_vec(out_psi, out_psi, in_psi, params.L);
   free(temp);
 }
@@ -26,12 +26,6 @@ static void f_psi(double complex *out_psi, double complex *in_psi, parameters pa
 void step_cn(double complex *out, double complex *in, parameters params) {
   double complex *temp = malloc(params.L * sizeof(double complex));
   cg(temp, f_eta, in, params);
-  f_psi(out, temp, params); // maybe this does not like out, out. not tested yet
+  f_psi(out, temp, params);
   free(temp);
-}
-
-void cn(double complex *out, double complex *in, parameters params){
-  for(long int t=0; (t * params.tau) < params.total_time; t++){
-    step_cn(out, in, params);
-  }
 }
