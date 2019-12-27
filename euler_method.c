a07b2c0a7dfe3d7b548f7c85cf1954a1fe6310d3
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-27 13:58:10 +0100

    Euler method computes only single steps now. The for loop has to be in the function thats calling euler method

--- euler_method.c
+++ euler_method.c
@@ -16,20 +16,12 @@ void euler_method(double complex *out, double complex *in, parameters params) {
     // Allocate memory for a temporary array of length L
     double complex *ham_out = malloc(params.L*sizeof(double complex));
 
-    // Copy data from in array to out array (= 0 time steps calculated)
-    for(long int i=0; i<params.L; i++){
-        out[i] = in[i];
-    }
+    // Calculate H(psi)
+    hamiltonian(ham_out, out, params);
 
-    // For loop that is being executed until the simulation length specified in total_length (to be given in 1/E_H approx 2.4 * 10^{-17} s) is reached
-    for(long int t=0; (t * params.tau) < params.total_time; t++){
-        // Calculate H(psi)
-        hamiltonian(ham_out, out, params);
-        // psi^{q+1} = psi^q - i tau H(psi^q)
-        for(long int i=0; i<params.L; i++){
-            out[i] -= 1I * params.tau * ham_out[i];
-        }
-        printf("%li\n",t);
+    // psi^{q+1} = psi^q - i tau H(psi^q)
+    for(long int i=0; i<params.L; i++){
+        out[i] = in[i] - 1I * params.tauhat * ham_out[i];
     }
 
     free(ham_out);
