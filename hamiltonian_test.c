92e1bf13f22de79a18f9fc42a9e7c67f50de3b67
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-27 13:59:25 +0100

    Bugfixes to get test program running. Not properly working yet

--- hamiltonian_test.c
+++ hamiltonian_test.c
@@ -16,23 +16,23 @@ int main(){
 
     parameters params;
 
-    double omega = 8.3e14;
-    double mass_H = 1.7e-27;
-    double hbar = 1.054571817e-34;
+    double omega = 8.3e14; //Hz
+    double mass_H = 1.67e-27; //kg
+    double hbar = 1.054571817e-34; //Js
 
     params.N = 1001;
     params.D = 1;
     params.L = ipow(params.N, params.D);
-    params.tau = 0.1;
     params.tol = DBL_EPSILON;
     params.max_iter = 1000;
-    params.total_time = 0.1;
-    params.epsilon = hbar * omega;
-    params.ext_potential_type = 0;
-    params.mhat = 0.001;
-    params.a = hbar * sqrt(2*params.mhat/(mass_H * params.epsilon));
 
-    double complex *out = malloc(params.L * sizeof(double complex));
+    params.tauhat = 0.1;
+    params.epsilon = hbar * omega;
+    params.mhat = 5e-4;
+    params.khat = params.mhat;
+    params.a = hbar * sqrt(params.mhat / (mass_H*params.epsilon));
+
+    double complex *out_wf = malloc(params.L * sizeof(double complex));
     double complex *start_wf = malloc(params.L * sizeof(double complex));
     double complex *x_space = malloc(params.L * sizeof(double complex));
     long int *coords = malloc(params.D * sizeof(long int));
@@ -48,36 +48,47 @@ int main(){
     }
 
     for(long int i=0; i<params.L; i++){
-        start_wf[i] = sqrt(sqrt(mass_H * omega / (M_PI * hbar))) * exp(-1/2. * mass_H * omega / hbar * x_space[i] * x_space[i]);
+        start_wf[i-100] = pow(sqrt(params.a),params.D) * sqrt(sqrt(mass_H * omega / (M_PI * hbar))) * exp(-1/2. * mass_H * omega / hbar * x_space[i] * x_space[i]);
     }
 
     parameters *p = &params;
     gen_pot_harmonic(p, omega);
 
-    euler_method(out, start_wf, params);
-
     startwf_file = fopen("h_test_input.txt","w");
-    potential_file = fopen("h_potential.txt","w");
+    for(long int i=0; i<params.L; i++){
+        fprintf(startwf_file, "%e\n", creal(start_wf[i])/*, cimag(start_wf[i])*/);
+    }
+    fclose(startwf_file);
+
     output_file = fopen("h_output.txt","w");
 
-    for(long int i=0; i<params.L; i++){
-        printf("%e%+e i\n", creal(out[i]), cimag(out[i]));
-        fprintf(startwf_file, "%e\n", creal(start_wf[i])/*, cimag(start_wf[i])*/);
-        fprintf(potential_file, "%e\n", creal(params.pot[i])/*, cimag(params.pot[i])*/);
-        fprintf(output_file, "%e\n", cabs(out[i])/*, cimag(params.pot[i])*/);
+    for(long int t=0; (t * params.tauhat) < 0.3; t++){
+        euler_method(out_wf, start_wf, params);
+        for(long int i=0; i<params.L; i++){
+            fprintf(output_file, "%e\t", cabs(out_wf[i]));
+        }
+        fprintf(output_file,"\n");
+        assign_vec(start_wf, out_wf, params.L);
     }
 
-    printf("%e",params.a);
-
-    fclose(startwf_file);
-    fclose(potential_file);
     fclose(output_file);
 
+    potential_file = fopen("h_potential.txt","w");
+
+    for(long int i=0; i<params.L; i++){
+        printf("%li: %e%+e i\n", i, creal(out_wf[i]), cimag(out_wf[i]));
+        fprintf(potential_file, "%e\n", creal(params.pot[i]));
+    }
+
+    printf("a = %e", params.a);
+
+    fclose(potential_file);
+
     free(coords);
-    free(out);
     free(start_wf);
     free(x_space);
     free(params.pot);
+    //free(out_wf);
 
     return 0;
 }
