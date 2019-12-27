67d3b02159a1f08c13d4e974c9cc71446aed3960
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-27 13:59:42 +0100

    Added previously removed variables

--- structs.h
+++ structs.h
@@ -11,10 +11,13 @@ typedef struct parameters_tag {
   unsigned int D; // number of dimensions
   long int L; // length of whole array
   double complex *pot; // space to store potential array
-  double tau; // length of time step
+  double tauhat; // length of time step
   double mhat; // natural parameter m^ = m*epsilon*a^2/hbar^2
+  double khat; // natural parameter k^ = m^ * hbar^2 *omega^2 / epsilon^2
   double tol; // numerical tolerance for solvers (atm. for to cg method)
   long int max_iter; // maximum number of iterations to perform (atm. for to cg method)
+  double epsilon; // energy scale
+  double a; // distance between two lattice points
 } parameters /*name to reference struct*/;
 
 #endif
