c66e12ac48e6a3e314d1e93b77dc828682bec5c0
androidmop <57952951+androidmop@users.noreply.github.com>
2019-12-29 12:54:44 +0100

    Create observables.h

---
+++ observables.h
@@ -0,0 +1,4 @@
+double complex obs_norm(double complex *in, parameters params);
+double complex obs_E(double complex *in, parameters params);
+double complex obs_x(double complex *in, unsigned int d, parameters params);
+double complex obs_p(double complex *in, unsigned int d, parameters params);
