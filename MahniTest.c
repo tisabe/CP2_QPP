41dbdb7e6561caf5d51db0607a998c9eedb5f500
IDungeon <57952984+IDungeon@users.noreply.github.com>
2020-01-02 19:58:23 +0100

    Update MahniTest.c

--- MahniTest.c
+++ MahniTest.c
@@ -18,13 +18,15 @@ int L = ipow(N, D);
 
     
     double complex *x= malloc(L * sizeof(double complex));
+double complex *x2= malloc(L * sizeof(double complex));
+double complex *x3= malloc(L * sizeof(double complex));
     
     for(int i=0; i<L; i++){
         x[i] = 10.0;
     }
-	nfft(x, x, N, D);
-	nfft_inverse(x, x, N, D);
+	nfft(x2, x, N, D);
+	nfft_inverse(x3, x2, N, D);
 for( int i=0; i < L; i++){
-		printf("x[i] \n");
+		printf("x3[i] \n");
 	}
 }
