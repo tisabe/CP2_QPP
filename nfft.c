210095d2d7b7865a27fb34843a28428156659acb
IDungeon <57952984+IDungeon@users.noreply.github.com>
2020-01-02 20:49:21 +0100

    Update nfft.c

--- nfft.c
+++ nfft.c
@@ -46,15 +46,18 @@ void nfft(double complex *out, double complex *in, int N, int D)
    /****************************************************************************
    VOLUME = N^D
    ****************************************************************************/
+   long int L=pow(N,D);
+   gsl_fft_complex_wavetable * wavetable;
+   gsl_fft_complex_workspace * workspace;
+
+   wavetable = gsl_fft_complex_wavetable_alloc (L);
+   workspace = gsl_fft_complex_workspace_alloc (L);
+
    int VOLUME=1;
    for(int j=0;j<D;j++) VOLUME*=N;
 
    memcpy(out,in,sizeof(double complex)*VOLUME);
-   gsl_fft_complex_wavetable * wavetable;
-   gsl_fft_complex_workspace * workspace;
-
-   wavetable = gsl_fft_complex_wavetable_alloc (VOLUME);
-   workspace = gsl_fft_complex_workspace_alloc (VOLUME);
+   
 
    int Nj=1;
    for(int j=0;j<D;j++)
@@ -87,16 +90,17 @@ void nfft_inverse(double complex *out, double complex *in, int N, int D)
    /****************************************************************************
    VOLUME = N^D
    ****************************************************************************/
+   long int L=pow(N,D);
+   gsl_fft_complex_wavetable * wavetable;
+   gsl_fft_complex_workspace * workspace;
+
+   wavetable = gsl_fft_complex_wavetable_alloc (L);
+   workspace = gsl_fft_complex_workspace_alloc (L);
+
    int VOLUME=1;
    for(int j=0;j<D;j++) VOLUME*=N;
 
    memcpy(out,in,sizeof(double complex)*VOLUME);
-   gsl_fft_complex_wavetable * wavetable;
-   gsl_fft_complex_workspace * workspace;
-
-   wavetable = gsl_fft_complex_wavetable_alloc (VOLUME);
-   workspace = gsl_fft_complex_workspace_alloc (VOLUME);
-
 
    int Nj=1;
    for(int j=0;j<D;j++)
