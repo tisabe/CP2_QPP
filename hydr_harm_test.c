880f229a7d8af822b08f936048f4c233d33b2088
androidmop <57952951+androidmop@users.noreply.github.com>
2020-01-06 22:51:18 +0100

    hydr_harm_test.c updated and executed

--- hydr_harm_test.c
+++ hydr_harm_test.c
@@ -21,24 +21,24 @@ int main(){
     double mass_H = 1.67e-27; //kg
     double hbar = 1.054571817e-34; //Js
 
-    params.N = 1001;
-    params.D = 2;
+    params.N = 2001;
+    params.D = 1;
     params.L = ipow(params.N, params.D);
     params.tol = DBL_EPSILON;
     params.max_iter = params.L;
 
     params.tauhat = 1e-3;
     params.epsilon = hbar * omega;
-    params.mhat = 5e-4;
-    params.khat = params.mhat;
-    params.a = hbar * sqrt(params.mhat / (mass_H*params.epsilon));
+    params.a = 2e-13;
+    params.mhat = pow(params.a/hbar,2)*mass_H*params.epsilon;     //=mass_H*omega*a^2/(2*hbar)
+    params.khat = params.mhat * pow(hbar*omega/params.epsilon,2); //=params.mhat
 
-    double simulation_duration = 1e-1;
+    double simulation_duration = 1e1;
     int number_time_steps = 100;
+    double offset[3] = {200,50,50};
 
     double complex *out_wf = malloc(params.L * sizeof(double complex));
     double complex *start_wf = malloc(params.L * sizeof(double complex));
-    double complex *x_space = malloc(params.L * sizeof(double complex));
     long int *coords = malloc(params.D * sizeof(long int));
     params.pot = malloc(params.L * sizeof(double complex));
 
@@ -47,13 +47,13 @@ int main(){
     FILE *norm_file;
     FILE *output_file;
 
-    for(long int i=0; i<params.L; i++){
-        index2coord(coords, i, params.N, params.D);
-        x_space[i] = coords[0] * params.a;
-    }
+    set_zero(start_wf, params.L);
 
     for(long int i=0; i<(params.L); i++){
-        start_wf[i] = pow(sqrt(params.a),params.D) * sqrt(sqrt(mass_H * omega / (M_PI * hbar))) * exp(-1/2. * mass_H * omega / hbar * pow(x_space[i] + 50*params.a, 2));
+        index2coord(coords, i, params.N, params.D);
+        for(int j=0; j<params.D; j++){
+            start_wf[i] += pow(sqrt(params.a),params.D) * sqrt(sqrt(params.mhat/M_PI)/params.a) * exp(-.5*params.mhat*pow(coords[j]+offset[j],2));
+        }
     }
 
     parameters *p = &params;
@@ -99,7 +99,6 @@ int main(){
 
     free(coords);
     free(start_wf);
-    free(x_space);
     free(params.pot);
     //free(out_wf);
 
