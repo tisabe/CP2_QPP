688276233f21cf5941059464ee5bf633d924a9a8
Mahni Mueller <mulermah@pool29.physik.hu-berlin.de>
2020-01-08 13:04:59 +0100

    update

--- nfft_test.c
+++ nfft_test.c
@@ -4,7 +4,8 @@
 #include <math.h>
 #include <complex.h>
 #include "nfft.h"
-
+/* in this short file we make a plausibility test of the nfft to see if any fatal errors occur when we dont use the radix2  (in one dimension)*/
+//function which generates a random vector "vec" of length L
 void ran_vec(double complex *vec, long int L) {
   /*Generates a random vector of size L and saves it in *vec.*/
   for (long int i=0; i<L; i++) {
@@ -12,7 +13,7 @@ void ran_vec(double complex *vec, long int L) {
   }
 }
   int main(){
-      
+      //declare paramters
       int N= 25600;
     int D = 1;
  long int L = pow(N, D);  
@@ -20,13 +21,17 @@ void ran_vec(double complex *vec, long int L) {
  double complex *vec = malloc(L* sizeof(double complex));
  double complex *vec_fft = malloc(L* sizeof(double complex));
  double complex *difference = malloc(L* sizeof(double complex));
+ 
+ //generate random vector
  ran_vec(vec, L);
- 
- 
- 
+ //apply fft on the random vector
  nfft(vec_fft,vec,N,D);
+ //apply the inverse fft 
  nfft_inverse(vec_fft,vec_fft,N,D);
  
+ 
+ // calculate the difference between the original random vector and the on after applying the fft and inverse fft. we expect every element to be 0
+ // write the result in .txt file "nfft_testfile.txt"
  FILE *f;
  f=fopen("nfft_testfile.txt","w");
  for(int i=0; i<L; i++) {
@@ -39,6 +44,7 @@ fclose(f);
 	free(vec_fft);
     free(difference);
     
+// generate a sin() function and apply the fft, the output in the textfile can be plotted to see if the receive desired frequency
     double complex *vec_sinus = malloc(L* sizeof(double complex));
 
     
